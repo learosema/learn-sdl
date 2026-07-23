5dd30b84cf4cb7ceb6e9771a855d8589739113e1
Lea Rosema <lea@lea.lgbt>
2026-07-23 21:48:32 +0200

    chore: intermediary commit

---
+++ 04_tiles/tiles-app.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <SDL3/SDL.h>
+
+class TilesApp {
+
+public:
+	~TilesApp();
+
+	SDL_AppResult Iterate();
+	
+	SDL_AppResult Init();
+
+	SDL_AppResult HandleEvent(const SDL_Event* const event);
+
+	inline const int GetWidth() const { return _width; }
+	inline const int GetHeight() const { return _height; }
+
+private:
+	int _width = 640;
+	int _height = 480;
+	bool _resized = true;
+
+	SDL_Window*   _window   = nullptr;
+	SDL_Renderer* _renderer = nullptr;
+
+};
