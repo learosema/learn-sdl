55bd036270602275036a3c8c5827f0e58c13c9a2
Lea Rosema <lea@lea.lgbt>
2026-08-07 09:35:48 +0200

    chore: intermediary commit

--- cmake/vsconfig.cmake
+++
@@ -1,6 +0,0 @@
-if (WIN32)
-  message(STATUS "Workaround so Windows will find the SDL3 DLL.")
-  set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR})
-  set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR})
-  set(CMAKE_WIN32_EXECUTABLE ON)
-endif()
